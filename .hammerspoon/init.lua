1dcee5f32a4e196e05c916743696e5d502547605
stordahl <jacob.stordahl@stylitics.com>
2023-01-03 21:08:45 -0600

    add hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,20 @@
+hs.loadSpoon('HCalendar')
+spoon.HCalendar:start()
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+--hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  --hs.alert.show("Hello World")
+--end)
