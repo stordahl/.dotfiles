bcef7a6ee13c3293e2f398e9d01258462c2e0e6a
stordahl <jacob.stordahl@stylitics.com>
2024-05-30 21:51:25 -0500

    feat: add keymap for telescope current changed files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,7 @@ vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", options)
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", options)
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", options)
+vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>lua require('telescope.builtin').git_status()<cr>", options)
 vim.keymap.set("n", "<leader>g", function ()
   builtin.grep_string({ search = vim.fn.input("grep :")})
 end)
